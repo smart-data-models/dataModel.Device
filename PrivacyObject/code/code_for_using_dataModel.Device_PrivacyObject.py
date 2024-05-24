7af4671c16b2c0d40593d9433945dd7ef74575d4
Alberto Abella <alberto.abella@fiware.org>
2024-05-24 17:22:15 +0200

    created example of code

--- PrivacyObject/code/code_for_using_dataModel.Device_PrivacyObject.py
+++ PrivacyObject/code/code_for_using_dataModel.Device_PrivacyObject.py
@@ -24,31 +24,36 @@
 #         curl -X GET http://localhost:1026/ngsi-ld/v1/entities?local=true&limit=1000
 #         
 #         # now the python code you can use to insert some value in the context broker according to the data model
+#         # Version Warning! 
+#         # This code is designed to work with the version 0.8 of pysmartdatamodels or later
+#         # to work with earlier version you need to replace the import instruction for
+#         # from pysmartdatamodels import pysmartdatamodels as sdm
 #         
-from pysmartdatamodels import pysmartdatamodels as sdm
+#         
+import pysmartdatamodels as sdm
 import subprocess
 serverUrl = "http://localhost:1026" # supposed that your broker is installed in localhost. Edit to match your configuration
 dataModel = "PrivacyObject"
 subject = "dataModel.Device"
-category = {'type': 'Property', 'value': ['sensor']}
+category = ['sensor']
 attribute = "category"
 value = category
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-crossborderTransfer = "{'type': 'Property', 'value': 'None'}"
+crossborderTransfer = "None"
 attribute = "crossborderTransfer"
 value = crossborderTransfer
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-floor = {'type': 'Property', 'value': 0}
+floor = 0
 attribute = "floor"
 value = floor
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-image = "{'type': 'Property', 'value': 'http://www.example.com/device1.jpg'}"
+image = "http://www.example.com/device1.jpg"
 attribute = "image"
 value = image
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
