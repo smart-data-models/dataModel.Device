8477a4f1dbef20a756a48c526fb1c88ec58e63e9
Alberto Abella <alberto.abella@fiware.org>
2024-05-24 17:22:18 +0200

    created example of code

--- SmartMeteringObservation/code/code_for_using_dataModel.Device_SmartMeteringObservation.py
+++ SmartMeteringObservation/code/code_for_using_dataModel.Device_SmartMeteringObservation.py
@@ -24,31 +24,36 @@
 #         curl -X GET http://localhost:1026/ngsi-ld/v1/entities?local=true&limit=1000
 #         
 #         # now the python code you can use to insert some value in the context broker according to the data model
+#         # Version Warning! 
+#         # This code is designed to work with the version 0.8 of pysmartdatamodels or later
+#         # to work with earlier version you need to replace the import instruction for
+#         # from pysmartdatamodels import pysmartdatamodels as sdm
 #         
-from pysmartdatamodels import pysmartdatamodels as sdm
+#         
+import pysmartdatamodels as sdm
 import subprocess
 serverUrl = "http://localhost:1026" # supposed that your broker is installed in localhost. Edit to match your configuration
 dataModel = "SmartMeteringObservation"
 subject = "dataModel.Device"
-entityVersion = "{'type': 'Property', 'value': '2.0'}"
+entityVersion = "2.0"
 attribute = "entityVersion"
 value = entityVersion
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-image = "{'type': 'Property', 'value': 'urn:ngsi:iVBORw0KGgoAAAANSUhEUgAAAGcAAABkCAIAAAAUt...ErkJggg=='}"
+image = "urn:ngsi:iVBORw0KGgoAAAANSUhEUgAAAGcAAABkCAIAAAAUt...ErkJggg=="
 attribute = "image"
 value = image
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-meterType = "{'type': 'Property', 'value': 'Electricity'}"
+meterType = "Electricity"
 attribute = "meterType"
 value = meterType
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-offPeakConsumption = {'type': 'Property', 'value': 8.0}
+offPeakConsumption = 8.0
 attribute = "offPeakConsumption"
 value = offPeakConsumption
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
