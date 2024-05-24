f20906a24650e58cddf1a97fee1921d840db60c1
Alberto Abella <alberto.abella@fiware.org>
2024-05-24 17:21:53 +0200

    created example of code

--- Camera/code/code_for_using_dataModel.Device_Camera.py
+++ Camera/code/code_for_using_dataModel.Device_Camera.py
@@ -24,31 +24,36 @@
 #         curl -X GET http://localhost:1026/ngsi-ld/v1/entities?local=true&limit=1000
 #         
 #         # now the python code you can use to insert some value in the context broker according to the data model
+#         # Version Warning! 
+#         # This code is designed to work with the version 0.8 of pysmartdatamodels or later
+#         # to work with earlier version you need to replace the import instruction for
+#         # from pysmartdatamodels import pysmartdatamodels as sdm
 #         
-from pysmartdatamodels import pysmartdatamodels as sdm
+#         
+import pysmartdatamodels as sdm
 import subprocess
 serverUrl = "http://localhost:1026" # supposed that your broker is installed in localhost. Edit to match your configuration
 dataModel = "Camera"
 subject = "dataModel.Device"
-cameraName = "{'type': 'Property', 'value': 'Cam2'}"
+cameraName = "Cam2"
 attribute = "cameraName"
 value = cameraName
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-cameraNum = {'type': 'Property', 'value': 2}
+cameraNum = 2
 attribute = "cameraNum"
 value = cameraNum
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-cameraOrientation = {'type': 'Property', 'value': {'comments': 'Camera facing RSBhawan', 'annotatedMap': 'https://drive.google.com/file/d/1RXDGogU5UMmzRppqFaNKTzcr69Kl0wjb/view?usp=sharing'}}
+cameraOrientation = {'comments': 'Camera facing RSBhawan', 'annotatedMap': 'https://drive.google.com/file/d/1RXDGogU5UMmzRppqFaNKTzcr69Kl0wjb/view?usp=sharing'}
 attribute = "cameraOrientation"
 value = cameraOrientation
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-cameraType = "{'type': 'Property', 'value': 'FIXED'}"
+cameraType = "FIXED"
 attribute = "cameraType"
 value = cameraType
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
